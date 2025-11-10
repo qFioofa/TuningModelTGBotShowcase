b2cf078809f6aa17c33246da8796617124812c1b
qFioofa <penavana1@gmail.com>
2025-11-10 17:25:38 +0300

    Bot info content added

--- message/text_types.py
+++ message/text_types.py
@@ -2,3 +2,8 @@ from message.message import nMessageType
 
 class TextType(nMessageType):
     START : str = "start"
+    GENERATE : str = "generate"
+    SET_MODEL : str = "set_model"
+
+    GENERATE_USAGE : str = "generate_usage"
+    SET_MODEL_USAGE : str = "set_model_usage"
